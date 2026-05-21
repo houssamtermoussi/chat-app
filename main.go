626eb419a4982367a71e337e442860214a6b3f60
houssamtermoussi <houssam111888@gmail.com>
2026-05-21 20:31:19 +0100

    websocket

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
     "net/http"
 )
 
+import ("github.com/gorilla/websocket")
+
 func home(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, "Chat App Go fonctionne")
 }
@@ -15,4 +17,10 @@ func main() {
     fmt.Println("Serveur lancé sur http://localhost:8080")
 
     http.ListenAndServe(":8080", nil)
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
